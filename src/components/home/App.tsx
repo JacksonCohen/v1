20e5a74d57548ae08e1feaf32e17025679f5e1bb
Jackson <jacksonrcohen@gmail.com>
2021-05-11 18:36:56 -0400

    Create component skeleton

--- src/components/home/App.tsx
+++ src/components/home/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Contact from './Contact';
+import Footer from './Footer';
+import Header from './Header';
+import Nav from './Nav';
+import Projects from './Projects';
+import Skills from './Skills';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
