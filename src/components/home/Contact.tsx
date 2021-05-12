30446b9562df49075f331cc042a6410e31fbc5a9
Jackson <jacksonrcohen@gmail.com>
2021-05-12 19:59:55 -0400

    Include emailjs and recaptcha

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,4 +1,6 @@
 import { styled } from '@stitches/react';
+import { ChangeEvent, useReducer } from 'react';
+import emailjs from 'emailjs-com';
 
 const Section = styled('section', {
   padding: '100px 0',
@@ -48,7 +50,60 @@ const Submit = styled('button', {
   marginLeft: 'auto',
 });
 
+interface State {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const initialState = {
+  name: '',
+  email: '',
+  message: '',
+};
+
+const stateReducer = (state: State, newState: any) => ({ ...state, ...newState });
+
 const Contact = () => {
+  const [userInput, setUserInput] = useReducer(stateReducer, initialState);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    const { name, value } = event.target;
+
+    setUserInput({ [name]: value });
+  };
+
+  const resetForm = () => {
+    setUserInput(initialState);
+  };
+
+  const sendEmail = () => {
+    const message = (
+      <>
+        <h1>Message from {userInput.name}</h1>
+
+        <div>{userInput.message}</div>
+        <div>You can reply at the following email: {userInput.email}</div>
+      </>
+    );
+    const templateParams = {
+      messageHtml: message,
+    };
+
+    emailjs
+      .send(
+        'jacksoncohen.dev',
+        'contactForm',
+        templateParams,
+        process.env.REACT_APP_EMAILJS_USER_ID
+      )
+      .then((res: any) => {
+        resetForm();
+        console.log(res, 'Success');
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <Section>
       <Wrapper>
@@ -57,10 +112,35 @@ const Contact = () => {
         <Subtitle>Have a question or want to work together? I'd love to hear from you!</Subtitle>
 
         <Form>
-          <Input placeholder='Name' />
-          <Input placeholder='Your email' />
-          <Textarea placeholder='Your message' />
-          <Submit type='submit'>Submit</Submit>
+          <Input
+            placeholder='Name'
+            name='name'
+            value={userInput.name}
+            onChange={handleChange}
+            required
+          />
+          <Input
+            placeholder='Your email'
+            name='email'
+            value={userInput.email}
+            onChange={handleChange}
+            required
+          />
+          <Textarea
+            placeholder='Your message'
+            name='message'
+            value={userInput.message}
+            onChange={handleChange}
+            required
+          />
+          <div
+            className='g-recaptcha'
+            data-sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+          ></div>
+
+          <Submit type='submit' onClick={sendEmail}>
+            Submit
+          </Submit>
         </Form>
       </Wrapper>
     </Section>
