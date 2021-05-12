27497bd0338bf3128d8825b6a92dbe31c5c0d0f7
Jackson <jacksonrcohen@gmail.com>
2021-05-12 19:59:14 -0400

    Create basic contact form and styled components

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import { styled } from '@stitches/react';
+
+const Section = styled('section', {
+  padding: '100px 0',
+  position: 'relative',
+  background: 'black',
+});
+
+const Wrapper = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+});
+
+const Title = styled('h2', {
+  fontSize: '1.8rem',
+  textTransform: 'uppercase',
+  color: 'white',
+});
+
+const Subtitle = styled('div', {
+  fontSize: '1rem',
+  color: 'teal',
+});
+
+const Form = styled('form', {
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  width: '33%',
+});
+
+const Input = styled('input', {});
+
+const Textarea = styled('textarea', {});
+
+const Submit = styled('button', {
+  color: 'white',
+  background: 'none',
+  border: '1px white solid',
+  borderRadius: '1px',
+  '&:hover': {
+    backgroundColor: 'teal',
+  },
+  padding: '10px 10px',
+  textTransform: 'uppercase',
+  marginLeft: 'auto',
+});
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <Section>
+      <Wrapper>
+        <Title>Contact</Title>
+
+        <Subtitle>Have a question or want to work together? I'd love to hear from you!</Subtitle>
+
+        <Form>
+          <Input placeholder='Name' />
+          <Input placeholder='Your email' />
+          <Textarea placeholder='Your message' />
+          <Submit type='submit'>Submit</Submit>
+        </Form>
+      </Wrapper>
+    </Section>
+  );
 };
 
 export default Contact;
