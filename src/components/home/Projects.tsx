2492d8461b57e1bcf77c18c80bb99912daa481f4
Jackson <jacksonrcohen@gmail.com>
2021-05-12 18:40:39 -0400

    Import and map over projects

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import Project from './Project';
 
 const Projects = () => {
-  return <div></div>;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('projects.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((projects) => {
+        setProjects(projects);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Projects</h2>
+
+      {projects.map((project) => (
+        <Project />
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
