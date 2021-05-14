d67470d7133a162141b477605ab77db8fb92d4a5
Jackson <jacksonrcohen@gmail.com>
2021-05-14 01:00:25 -0400

    Handle scroll in App

--- src/components/home/App.tsx
+++ src/components/home/App.tsx
@@ -5,23 +5,61 @@ import Navbar from './Navbar';
 import Projects from './Projects';
 import About from './About';
 import { global } from '@stitches/react';
-import { useSticky } from 'src/utils/useSticky';
+import { useEffect, useRef, useState } from 'react';
+import { getDimensions } from 'src/utils/getDimensions';
 
 const globalStyles = global({
   '*': { margin: 0, padding: 0, fontFamily: 'Open Sans' },
 });
 
 const App = () => {
-  const { isSticky, element } = useSticky();
+  const [visibleSection, setVisibleSection] = useState<string>();
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
   globalStyles();
+
+  useEffect(() => {
+    const sectionRefs = [
+      { section: 'About', ref: aboutRef },
+      { section: 'Projects', ref: projectsRef },
+      { section: 'Contact', ref: contactRef },
+    ];
+
+    const handleScroll = () => {
+      const { height: headerHeight } = getDimensions(homeRef.current);
+      const scrollPosition = window.scrollY + headerHeight;
+
+      const selected = sectionRefs.find(({ section, ref }) => {
+        const element = ref.current;
+        if (element) {
+          const { offsetBottom, offsetTop } = getDimensions(element);
+          return scrollPosition > offsetTop && scrollPosition < offsetBottom;
+        }
+      });
+
+      if (selected && selected.section !== visibleSection) {
+        setVisibleSection(selected.section);
+      } else if (!selected && visibleSection) {
+        setVisibleSection(undefined);
+      }
+    };
+
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [visibleSection]);
 
   return (
     <div>
-      <Header />
-      <Navbar sticky={isSticky} active={''} />
-      <About />
-      <Projects element={element} />
-      <Contact />
+      <Header homeRef={homeRef} />
+      <Navbar section={visibleSection} />
+      <About aboutRef={aboutRef} />
+      <Projects projectsRef={projectsRef} />
+      <Contact contactRef={contactRef} />
       <Footer />
     </div>
   );
