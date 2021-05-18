d49a1ca556f275a8facb53025fae57d2fa778b63
Jackson <jacksonrcohen@gmail.com>
2021-05-18 12:03:42 -0400

    Render navbar based on screen size

--- src/components/home/Navbar/Navbar.tsx
+++ src/components/home/Navbar/Navbar.tsx
@@ -1,36 +1,37 @@
-import { scrollTo } from 'src/utils/scrollTo';
-import { Nav, NavLink, Sticky } from './styles';
+import { ReactChild, useCallback, useEffect, useState } from 'react';
+import { DesktopNav } from 'src/components';
+import { Sticky } from './styles';
 
-interface NavProps {
+export interface NavProps {
   section: string;
 }
 
 const Navbar = ({ section }: NavProps) => {
-  return (
-    <Sticky>
-      <Nav>
-        <NavLink onClick={() => scrollTo('home')}>Home</NavLink>
-        <NavLink
-          onClick={() => scrollTo('about')}
-          className={section === 'About' ? 'selected' : ''}
-        >
-          About
-        </NavLink>
-        <NavLink
-          onClick={() => scrollTo('portfolio')}
-          className={section === 'Projects' ? 'selected' : ''}
-        >
-          Portfolio
-        </NavLink>
-        <NavLink
-          onClick={() => scrollTo('contact')}
-          className={section === 'Contact' ? 'selected' : ''}
-        >
-          Contact
-        </NavLink>
-      </Nav>
-    </Sticky>
+  const [nav, setNav] = useState<ReactChild>(<DesktopNav section={section} />);
+  const mql = window.matchMedia('(max-width: 600px)');
+
+  const setResponsiveNav = useCallback(
+    (event: MediaQueryListEvent) => {
+      const mobileView = event.matches;
+
+      if (mobileView) {
+        setNav(<></>);
+      } else {
+        setNav(<DesktopNav section={section} />);
+      }
+    },
+    [section]
   );
+
+  useEffect(() => {
+    mql.addEventListener('change', setResponsiveNav);
+
+    return () => {
+      mql.removeEventListener('change', setResponsiveNav);
+    };
+  }, [mql, section, setResponsiveNav]);
+
+  return <Sticky>{nav}</Sticky>;
 };
 
 export default Navbar;
