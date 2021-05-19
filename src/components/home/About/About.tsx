e5aec8809132eefbe39f63925d4a956dc85f69c7
Jackson <jacksonrcohen@gmail.com>
2021-05-19 15:03:30 -0400

    Add WIP About content and styling

--- src/components/home/About/About.tsx
+++ src/components/home/About/About.tsx
@@ -1,6 +1,6 @@
 import { MutableRefObject } from 'react';
 import { Title, TitleBar } from 'src/shared/styles';
-import { Section, Wrapper } from './styles';
+import { Content, List, ListItem, Paragraph, Picture, Section, Text, Wrapper } from './styles';
 import { useInView } from 'react-intersection-observer';
 
 interface AboutProps {
@@ -11,12 +11,68 @@ const About = ({ aboutRef }: AboutProps) => {
   const [inViewRef, inView] = useInView({ triggerOnce: true });
 
   return (
-    <Section id='about' ref={aboutRef}>
+    <Section id='about' ref={aboutRef} padding={{ '@bp0': 'sm', '@bp1': 'md' }}>
       <Wrapper ref={inViewRef}>
         <Title fontSize={{ '@bp0': 'sm', '@bp1': 'md' }} className={inView ? 'slide-right' : ''}>
           About
         </Title>
         <TitleBar height={{ '@bp0': 'sm', '@bp1': 'md' }} className={inView ? 'slide-left' : ''} />
+
+        <Content>
+          <Text>
+            <Paragraph>
+              Hi again! My name is Jackson and I'm a New York based software engineer specialized in
+              building (and occasionally designing) things for the web. My interest in web
+              development came on hard and fast and I haven't looked back since. I believe that
+              technology should improve peoples' lives and I am constantly studying to improve my
+              knowledge, overcome new challenges, and become a better and better developer.
+            </Paragraph>
+
+            <Paragraph>
+              Here are a few of the technologies I've been working with recently:
+            </Paragraph>
+            <List className='fa-ul'>
+              <ListItem>
+                <span className='fa-li'>
+                  <i className='fas fa-angle-right'></i>
+                </span>
+                JavaScript (ES6+)
+              </ListItem>
+              <ListItem>
+                <span className='fa-li'>
+                  <i className='fas fa-angle-right'></i>
+                </span>
+                TypeScript
+              </ListItem>
+              <ListItem>
+                <span className='fa-li'>
+                  <i className='fas fa-angle-right'></i>
+                </span>
+                React
+              </ListItem>
+              <ListItem>
+                <span className='fa-li'>
+                  <i className='fas fa-angle-right'></i>
+                </span>
+                Node.js
+              </ListItem>
+              <ListItem>
+                <span className='fa-li'>
+                  <i className='fas fa-angle-right'></i>
+                </span>
+                MySQL
+              </ListItem>
+              <ListItem>
+                <span className='fa-li'>
+                  <i className='fas fa-angle-right'></i>
+                </span>
+                GraphQL
+              </ListItem>
+            </List>
+          </Text>
+
+          <Picture src='https://via.placeholder.com/300x300' />
+        </Content>
       </Wrapper>
     </Section>
   );
--- src/components/home/About/styles.ts
+++ src/components/home/About/styles.ts
@@ -1,4 +1,10 @@
-import { styled } from 'src/stitches.config';
+import { keyframes, styled } from 'src/stitches.config';
+
+const pulse = keyframes({
+  '0%': { transform: 'scale(1)' },
+  '50%': { transform: 'scale(1.02)' },
+  '100%': { transform: 'scale(1)' },
+});
 
 export const Section = styled('section', {
   padding: '100px 200px',
@@ -22,4 +28,40 @@ export const Wrapper = styled('div', {
   justifyContent: 'center',
   alignItems: 'center',
   flexDirection: 'column',
+});
+
+export const Content = styled('div', {
+  display: 'flex',
+  width: '80%',
+
+  '& > *': {
+    margin: '0 20px',
+  },
+});
+
+export const Text = styled('div', {});
+
+export const Paragraph = styled('p', {
+  marginBottom: '1rem',
+});
+
+export const Picture = styled('img', {});
+
+export const List = styled('ul', {
+  columnCount: 2,
+  columnGap: '0',
+  width: '50%',
+});
+
+export const ListItem = styled('li', {
+  marginLeft: '15px',
+
+  '& span': {
+    color: '$accent0',
+  },
+
+  '&:hover': {
+    animation: `${pulse} 1s`,
+    backfaceVisibility: 'hidden',
+  },
 });
